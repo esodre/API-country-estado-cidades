012c76af9d2c78bce530af8f9a94728e0d1a8d7c
Erlon Paim Sodre <epsodre@acbeumaplebear.com.br>
2021-10-28 10:23:10 -0300

    First commit

---
+++ src/main/java/com/esodre/citiesapi/countries/Country.java
@@ -0,0 +1,47 @@
+package com.esodre.citiesapi.countries;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "pais")
+public class Country {
+    @Id
+    private Long id;
+
+    @Column(name = "nome")
+    private String nome;
+
+    @Column(name = "nome_pt")
+    private  String nomePortugues;
+
+    @Column(name = "sigla")
+    private String code;
+
+    private Integer bacen;
+
+    public Country() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getNomePortugues() {
+        return nomePortugues;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public Integer getBacen() {
+        return bacen;
+    }
+}
