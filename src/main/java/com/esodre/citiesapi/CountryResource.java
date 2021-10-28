fb31ad4d07b33b705000b0b2d3c46ea2fde0b6f5
Erlon Paim Sodre <epsodre@acbeumaplebear.com.br>
2021-10-28 10:31:25 -0300

    Buscar pais pelo ID

--- src/main/java/com/esodre/citiesapi/CountryResource.java
+++ src/main/java/com/esodre/citiesapi/CountryResource.java
@@ -6,10 +6,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/countries")
@@ -21,5 +23,11 @@ public class CountryResource {
     @GetMapping
     public Page<Country> countries(Pageable page){
         return repository.findAll(page);
+    }
+
+    @GetMapping("/{id}")
+    public Country getOne(@PathVariable Long id){
+        Optional <Country> optional = repository.findById(id);
+        return optional.get();
     }
 }
